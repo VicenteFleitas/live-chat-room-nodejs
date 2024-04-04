0d1d4326018da420344be7fb8da9bad8be742891
VicenteFleitas <vicente1fleitas1@gmail.com>
2024-04-04 00:15:30 -0400

    feat: server done

---
+++ index.mjs
@@ -0,0 +1,3 @@
+import { SkillShareServer } from "./server.mjs";
+
+new SkillShareServer({}).start(8000);
--- server.mjs
+++ server.mjs
@@ -18,6 +18,18 @@ router.add("GET", talkPath, async (server, title) => {
   }
 });
 
+router.add("GET", /^\/talks$/, async (server, request) => {
+  let tag = /"(.*)"/.exec(request.headers["if-none-match"]);
+  let wait = /\bwait=(\d+)/.exec(request.headers["prefer"]);
+  if (!tag || tag[1] != server.version) {
+    return server.talkResponse();
+  } else if (!wait) {
+    return { status: 304 };
+  } else {
+    return server.waitForChanges(Number(wait[1]));
+  }
+});
+
 router.add("DELETE", talkPath, async (server, title) => {
   if (Object.hasOwn(server.talks, title)) {
     delete server.talks[title];
@@ -82,7 +94,7 @@ function notFound(request, response) {
   response.end("<h1>Not found</h1>");
 }
 
-class SkillShareServer {
+export class SkillShareServer {
   constructor(talks) {
     this.talks = talks;
     this.version = 0;
@@ -101,3 +113,33 @@ class SkillShareServer {
     this.server.close();
   }
 }
+
+SkillShareServer.prototype.talkResponse = function () {
+  let talks = Object.keys(this.talks).map((title) => this.talks[title]);
+  return {
+    body: JSON.stringify(talks),
+    headers: {
+      "Content-Type": "application/json",
+      ETag: `"${this.version}"`,
+      "Cache-Control": "no-store",
+    },
+  };
+};
+
+SkillShareServer.prototype.waitForChanges = function (time) {
+  return new Promise((resolve) => {
+    this.waiting.push(resolve);
+    setTimeout(() => {
+      if (!this.waiting.includes(resolve)) return;
+      this.waiting = this.waiting.filter((r) => r != resolve);
+      resolve({ status: 304 });
+    }, time * 1000);
+  });
+};
+
+SkillShareServer.prototype.updated = function () {
+  this.version++;
+  let response = this.talkResponse();
+  this.waiting.forEach((resolve) => resolve(response));
+  this.waiting = [];
+};
